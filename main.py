67e240f50d47737165cb35e6cd06fa02063ab511
Nahuel <nahuel.developer1@gmail.com>
2024-07-08 12:42:14 -0300

    mensaje de proyecto subido a GitHub agregado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("hola Analítica Noviembre, soy Nahuel tu profe")
 print("El mejor bootcamp de data, todos trabajan después")
 print("En este bootcamp, los alumnos van a trabajar en data")
+print("Proyecto subido a GitHub")
 
